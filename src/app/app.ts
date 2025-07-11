3db42389c98404040af08fe565ea81e8d301f3a5
Martin Bartolomé <martin-bartolome@outlook.com>
2025-07-11 22:09:54 +0200

    Init

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  imports: [
+
+  ],
+  styleUrls: ['./app.css']
+})
+export class App {
+  title = 'praxis-lorena-bartolome';
+}
